2c863df296f1c5b79c881b605bc7181d868264e2
JacekB <jacekborko@gmail.com>
2026-04-19 19:45:46 +0200

    Project init

---
+++ src/main/kotlin/com/jb/coupons/CouponsApplication.kt
@@ -0,0 +1,11 @@
+package com.jb.coupons
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CouponsApplication
+
+fun main(args: Array<String>) {
+    runApplication<CouponsApplication>(*args)
+}
---
+++ src/test/kotlin/com/jb/coupons/CouponsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jb.coupons
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CouponsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
